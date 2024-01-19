026dbcb67302e7b3b4f53d02febf5a919a3b4a67
al3417 <che-jui-liu@outlook.com>
2024-01-18 19:35:02 -0800

    update

--- 13spellingbee.sh
+++ 13spellingbee.sh
@@ -1,12 +1,5 @@
+
 cd ~/Code/MCB185/data
 
-gunzip -c dictionary.gz | grep -E "^r+[zonica]{3,}$"
-gunzip -c dictionary.gz | grep -E "^[zonica]+r+[zonica]{2,}$"
-gunzip -c dictionary.gz | grep -E "^[zonica]{2,}r+[zonica]$"
-gunzip -c dictionary.gz | grep -E "^[zonica]{3,}r+$"
-gunzip -c dictionary.gz | grep -E "^r+[zonica]{2,}r+$"
-gunzip -c dictionary.gz | grep -E "^r+[zonica]+r+[zonica]+$"
-gunzip -c dictionary.gz | grep -E "^[zonica]+r+[zonica]+r+$"
-gunzip -c dictionary.gz | grep -E "^r{2,}[zonica]{2,}$"
-gunzip -c dictionary.gz | grep -E "^[zonica]{2,}r{2,}$"
+gunzip -c dictionary.gz | grep 'r' | grep -v '[bdefghjklmpqstuvwxy]'| grep -E '.{4,}'
 
