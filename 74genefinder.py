e52f1d07202680b719c1d3550122480b3ea9a6ad
al3417 <che-jui-liu@outlook.com>
2024-03-10 22:23:58 -0700

    update

--- 74genefinder.py
+++ 74genefinder.py
@@ -27,57 +27,40 @@ def translate_frames(frames):
 		aa_frames.append(dogma.translate(frame))
 	return aa_frames
 
-def find_orfs(seq, min_length):
-	orfs = []
-	frames = generate_frames(seq)
-	aa_frames = translate_frames(frames)
-
-	index = 0
-	for aa_seq in aa_frames:
-		frame_offset = index % 3 # 0, 1, 2
-		if index < 3:
-			strand = '+'
-		else:
-			strand = '-'
-		i = 0
-
-		while i < len(aa_seq):
-			if aa_seq[i] == 'M':
-				end_pos = aa_seq[i:].find('*')  # find * after M and new value for end
-				if end_pos != -1:
-					start_nt = (i * 3) + frame_offset
-					end_nt = (i + end_pos) * 3 + frame_offset
-
-					if (end_nt - start_nt + 1) >= min_length:
-						orf_table = {
-							'start': start_nt,
-							'end': end_nt,
-							'strand': strand,
-						}
-						orfs.append(orf_table)	# list of paired value
-
-					i += end_pos + 1  # move i after *
-				else:
-					break  
-			else:
-				i += 1
-
-		index += 1  
-
-	return orfs
-
-# I use the GFF feature from wikipedia
 def genefinder(fasta, min_length):
 	for name, seq in mcb185.read_fasta(fasta):
-		orfs = find_orfs(seq, min_length)
+		frames = generate_frames(seq)
+		aa_frames = translate_frames(frames)
+
+		index = 0
 		i = 1
-		for orf in orfs:	# each paired value
-			start = orf['start']
-			end = orf['end']
-			strand = orf['strand']
-			gff_line = f"{name}\tgenefinder\tCDS\t{start+1}\t{end+1}\t.\t{strand}\t0\tID={i}"
-			print(gff_line)
-			i += 1
+		for aa_seq in aa_frames:
+			frame_offset = index % 3
+			if index < 3:
+				strand = '+'
+			else:
+				strand = '-'
+
+			pos = 0
+			while pos < len(aa_seq):
+				if aa_seq[pos] == 'M':
+					end_pos = aa_seq[pos:].find('*')  # find * after M
+					if end_pos != -1:
+						start = (pos * 3) + frame_offset
+						end = (pos + end_pos) * 3 + frame_offset
+
+						if (end - start + 1) >= min_length:
+							gff_line = f"{name}\tgenefinder\tCDS\t{start + 1}\t{end + 1}\t.\t{strand}\t0\tID={i}"
+							print(gff_line)
+							i += 1
+
+						pos += end_pos + 1  # move pos after *
+					else:
+						break
+				else:
+					pos += 1
+
+			index += 1
 
 genefinder(fasta, min_length)
 
